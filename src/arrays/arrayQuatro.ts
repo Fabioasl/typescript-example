fd0aa219b377377ace500771144c1c221b697b0a
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-06-07 16:21:17 -0300

    remove: Deleta src/arrays/arrayQuatro.ts

--- src/arrays/arrayQuatro.ts
+++
@@ -1,8 +0,0 @@
-export function arrayQuatro(arr : number[]) : number[] {
-    if(arr.length < 2){
-        throw new Error("Tamanho do array deve ser no mínimo 2.");
-    }    
-    let array = arr.slice(0,2);
-    return array;
-
-}
