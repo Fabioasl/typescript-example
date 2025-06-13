7267dc37631b51ef71f3293dd7861df9f621cd7a
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-06-13 15:35:24 +0000

    feat: Adiciona código para a interface abstrata

---
+++ src/Interfaces/Interface_abstrata/Iatleta.test.ts
@@ -0,0 +1,64 @@
+import {Judoca, Basquetebolista } from './Iatleta';
+
+describe('Classe abstrata do judoca', () =>{
+    let judoca: Judoca;
+
+    beforeEach(() => {
+        judoca = new Judoca("Ana", 1.68, 65, "Verde");
+    });
+    it('Deve criar um judoca com os seguintes atributos', () => {
+        expect(judoca.nome).toBe("Ana");
+        expect(judoca.altura).toBe(1.68);
+        expect(judoca.peso).toBe(65);
+        expect(judoca.faixa).toBe("Verde");
+        expect(judoca.Categoria()).toBe(`Categoria do atleta: Ana é Médio.`)
+    });
+    
+    it('deve alterar os atributos do judoca' , () =>{
+        judoca.peso = 60;
+        judoca.faixa = "preta";
+
+        expect(judoca.peso).toBe(60);
+        expect(judoca.faixa).toBe("preta");
+        expect(judoca.Categoria()).toBe(`Categoria do atleta: Ana é Ligeiro.`)
+    });
+    it('A média de ana deve ser 100%', () =>{
+        expect(Judoca.media(judoca,10,0)).toBe(`A média de aproveitamento do atleta: Ana é 100%.`)
+    })
+    it('A média de ana deve ser: Não há confrontos registrados.', () =>{
+        expect(Judoca.media(judoca,0,0)).toBe(`Não há confrontos registrados.`)
+    })
+})
+
+describe('Classe abstrata para Basquetebolista', () =>{
+    let basquetebolista: Basquetebolista;
+    beforeEach(() => {
+        basquetebolista = new Basquetebolista("2pac", 1.76, 15, 15, 12, 6);
+    });
+    it ("O basquetebolista deve ter os seguintes atributos", () => {
+        expect(basquetebolista.nome).toBe("2pac");
+        expect(basquetebolista.altura).toBe(1.76);
+        expect(basquetebolista.pontos).toBe(15);
+        expect(basquetebolista.rebotes).toBe(15);
+        expect(basquetebolista.assistencias).toBe(12);
+        expect(basquetebolista.bloqueios).toBe(6);
+        expect(basquetebolista.Overall(3)).toBe(`O overall do atleta: 2pac é de 5 pontos, 5 rebotes, 4 assitencias, e 2 bloqueios em  um total de 3 partidas.`)
+    });
+    it ("Deve alterar os atributos do basquetebolista", () =>{
+        basquetebolista.pontos = 30;
+        basquetebolista.rebotes = 24;
+        basquetebolista.assistencias = 18;
+        basquetebolista.bloqueios = 12;
+        expect(basquetebolista.pontos).toBe(30);
+        expect(basquetebolista.rebotes).toBe(24);
+        expect(basquetebolista.assistencias).toBe(18);
+        expect(basquetebolista.bloqueios).toBe(12);
+        expect(basquetebolista.Overall(3)).toBe(`O overall do atleta: 2pac é de 10 pontos, 8 rebotes, 6 assitencias, e 4 bloqueios em  um total de 3 partidas.`)
+    })
+    it('A média de 2pac deve ser 100%', () =>{
+        expect(Basquetebolista.media(basquetebolista,3,0)).toBe(`A média de aproveitamento do atleta: 2pac é 100%.`)
+    })
+    it('A média de 2pac deve ser Não há confrontos registrados.', () =>{
+        expect(Basquetebolista.media(basquetebolista,0,0)).toBe(`Não há confrontos registrados.`)
+    })
+})
---
+++ src/Interfaces/Interface_abstrata/Iatleta.ts
@@ -0,0 +1,71 @@
+export abstract class Iatleta{
+    nome: string;
+    altura: number;
+
+    constructor(nome: string, altura: number) {
+        this.nome = nome;
+        this.altura = altura;
+    }
+    
+    static media(atleta: Iatleta, vitorias: number, derrotas: number): string {
+        const total = vitorias + derrotas;
+        if (total === 0) {
+        return "Não há confrontos registrados.";
+        }
+        const desempenho = (vitorias / total) * 100;
+        return `A média de aproveitamento do atleta: ${atleta.nome} é ${desempenho}%.`;
+    }
+}
+
+
+export class Judoca extends Iatleta {
+    faixa: string;
+    peso: number;
+    categoria: string;
+
+    constructor(nome: string, altura: number, peso: number, faixa: string) {
+        super(nome, altura);
+        this.peso = peso;
+        this.faixa = faixa;
+        this.categoria = "nenhuma";
+    }
+    Categoria() : string{
+        if (this.peso <= 60){
+            this.categoria = "Ligeiro";
+        }
+        else if (this.peso <= 80){
+            this.categoria = "Médio";
+        }
+        else{
+            this.categoria = "Pesado";
+        }
+        return `Categoria do atleta: ${this.nome} é ${this.categoria}.`
+    }
+}
+
+
+export class Basquetebolista extends Iatleta{
+    pontos: number;
+    rebotes: number;
+    assistencias: number;
+    bloqueios: number;
+
+        constructor(nome: string, altura: number, pontos: number, rebotes: number, assistencias: number, bloqueios: number) {
+        super(nome, altura);
+        this.pontos = pontos ;
+        this.rebotes = rebotes;
+        this.assistencias = assistencias;
+        this.bloqueios = bloqueios;
+    }
+    Overall(partidas :  number ) : string{
+        let total = partidas;
+        let av_pontos = this.pontos / partidas;
+        let av_rebotes = this.rebotes / partidas ;
+        let av_assistencias = this.assistencias / partidas ;
+        let av_bloqueios = this.bloqueios / partidas ;
+
+        return `O overall do atleta: ${this.nome} é de ${av_pontos} pontos, ${av_rebotes} rebotes, ${av_assistencias} assitencias, e ${av_bloqueios} bloqueios em  um total de ${total} partidas.`
+    }
+
+    
+}
