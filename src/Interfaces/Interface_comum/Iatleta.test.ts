a402f78c333282a0c433d0fd5b94f51a898e77ad
Fábio Alexandre <163214715+Fabioasl@users.noreply.github.com>
2025-06-13 13:26:53 +0000

    feat: Adiciona código para a interface comum

---
+++ src/Interfaces/Interface_comum/Iatleta.test.ts
@@ -0,0 +1,50 @@
+import {Judo, Basquete} from './Iatleta';
+
+describe('Classe judo', () => {
+    let judoca: Judo;
+
+    beforeEach(() => {
+        judoca = new Judo("Ferreira", 1.75, 75, "Roxa", "Meio-médio");
+    });
+
+    it('Deve criar um judoca com os seguintes atributos', () => {
+        expect(judoca.nome).toBe("Ferreira");
+        expect(judoca.altura).toBe(1.75);
+        expect(judoca.peso).toBe(75);
+        expect(judoca.faixa).toBe("Roxa");
+        expect(judoca.categoria).toBe("Meio-médio");
+    });
+    it('Deve alterar os atributos do judoca ja criado', () =>{
+        judoca.peso = 70;
+        judoca.categoria = "Leve";
+        expect(judoca.peso).toBe(70);
+        expect(judoca.categoria).toBe("Leve");
+    })
+});
+
+describe('Classe Basquete', () => {
+    let basquetebolista: Basquete;
+
+    beforeEach(() => {
+        basquetebolista = new Basquete("Lucena", 1.86, 7, 5, 3, 1 );
+    });
+
+    it('Deve criar um basquetebolista com os seguintes atributos', () => {
+        expect(basquetebolista.nome).toBe("Lucena");
+        expect(basquetebolista.altura).toBe(1.86);
+        expect(basquetebolista.pontos).toBe(7);
+        expect(basquetebolista.rebotes).toBe(5)
+        expect(basquetebolista.assitencias).toBe(3);
+        expect(basquetebolista.bloqueios).toBe(1);
+    });
+    it('Deve alterar os atributos do basquetebolista ja criado', () =>{
+        basquetebolista.pontos = 10;
+        basquetebolista.rebotes = 15;
+        basquetebolista.assitencias = 8;
+        basquetebolista.bloqueios = 5;
+        expect(basquetebolista.pontos).toBe(10);
+        expect(basquetebolista.rebotes).toBe(15)
+        expect(basquetebolista.assitencias).toBe(8);
+        expect(basquetebolista.bloqueios).toBe(5);
+    })
+});
---
+++ src/Interfaces/Interface_comum/Iatleta.ts
@@ -0,0 +1,38 @@
+export interface Iatleta{
+    nome: string;
+    altura: number;
+}
+
+export class Judo implements Iatleta{
+    nome: string;
+    altura: number;
+    peso: number;
+    faixa: string;
+    categoria: string;
+
+    constructor(nome: string, altura: number, peso: number, faixa: string, categoria: string){
+        this.nome = nome;
+        this.altura = altura;
+        this.peso = peso;
+        this.faixa = faixa;
+        this.categoria = categoria;
+    }
+}
+
+export class Basquete implements Iatleta{
+    nome: string;
+    altura: number;
+    pontos: number;
+    rebotes: number;
+    assitencias: number;
+    bloqueios: number;
+
+    constructor(nome: string, altura: number, pontos: number, rebotes: number, assitencias: number, bloqueios: number){
+        this.nome = nome;
+        this.altura = altura;
+        this.pontos = pontos;
+        this.rebotes = rebotes;
+        this.assitencias = assitencias;
+        this.bloqueios = bloqueios;
+    }
+}
